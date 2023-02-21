79079cf27da1954d9568fdcc14e47ffd48fcd30e
Kati Lassila-Perini <k.lassila-perini@cern.ch>
2023-02-21 21:26:28 +0100

    test github env variables

--- read_metadata.py
+++ read_metadata.py
@@ -13,13 +13,13 @@ def main():
     # for key, value in all_meta.items() :
     #    print (key, value)
 
-    print("export GT=" + all_meta['system_details']['global_tag'])
-    print("export RELEASE=" + all_meta['system_details']['release'])
+    print("GT=" + all_meta['system_details']['global_tag'])
+    print("RELEASE=" + all_meta['system_details']['release'])
     type = all_meta['type']['secondary'][0]
-    print("export TYPE=" + type) #collision or simulated
+    print("TYPE=" + type) #collision or simulated
     if type == "Collision": 
-      print("export VALI_RECID=" + all_meta['note']['links'][0]['recid']) #recid of validated runs file
-    print("export FORMAT=" + all_meta['distribution']['formats'][0]) #this assumes alphabetical order, do better
+      print("VALI_RECID=" + all_meta['note']['links'][0]['recid']) #recid of validated runs file
+    print("FORMAT=" + all_meta['distribution']['formats'][0]) #this assumes alphabetical order, do better
     #print(all_meta['files'][0]['uri']) #get these directly with cernopendata-client
 
 
